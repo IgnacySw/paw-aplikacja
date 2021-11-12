2ccc12c27e366236896dbaebc1a528da8190f717
IgnacySw <80746345+IgnacySw@users.noreply.github.com>
2021-11-12 18:32:16 +0100

    Add files via upload

---
+++ kred_0/src/main/java/com/jsfcourse/calc/kredytBB.java
@@ -0,0 +1,85 @@
+package com.jsfcourse.calc;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.enterprise.context.RequestScoped;
+import javax.enterprise.context.SessionScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+
+
+@Named
+@RequestScoped
+//@SessionScoped
+public class kredytBB{
+	private String x;
+	private String y;
+	private Double result;
+
+	@Inject
+	FacesContext ctx;
+
+	
+	
+	
+//	kwota 
+//	ile lat
+//	miesieczna rata = result
+	
+	
+	
+
+	public String getX() {
+		return x;
+	}
+
+	public void setX(String x) {
+		this.x = x;
+	}
+
+	public String getY() {
+		return y;
+	}
+
+	public void setY(String y) {
+		this.y = y;
+	}
+
+	public Double getResult() {
+		return result;
+	}
+
+	public void setResult(Double result) {
+		this.result = result;
+	}
+
+	
+	
+	public boolean doTheMath() {
+		try {
+			double x = Double.parseDouble(this.x);
+			double y = Double.parseDouble(this.y);
+
+			result = ( (x / y) + ( (1/10) * x ) );
+
+			ctx.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Operacja wykonana poprawnie", null));
+			return true;
+		} catch (Exception e) {
+			ctx.addMessage(null,
+					new FacesMessage(FacesMessage.SEVERITY_ERROR, "Błąd podczas przetwarzania parametrów", null));
+			return false;
+		}
+	}
+
+	
+	
+	// Go to "showresult" if ok
+	public String calc() {
+		if (doTheMath()) {
+			return "showresult";
+		}
+		return null;
+	}
+
+
+}
